21830b4999f02e73b068860a6fcc6837e3636fca
FomichevAYu <Mailforspoify@yandex.ru>
2023-04-26 23:14:26 +0300

    add controller, mapping by car, add car.html

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,14 @@
+package web.model;
+
+public class Car {
+    private String model;
+    private int series;
+    private String color;
+
+    public Car(String model, int series, String color) {
+        this.model = model;
+        this.series = series;
+        this.color = color;
+    }
+
+}
