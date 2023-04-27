c071b24cd6dc5bdd75d04360679c3a74b2add89c
FomichevAYu <Mailforspoify@yandex.ru>
2023-04-27 13:56:58 +0300

    final commit message

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -10,28 +10,14 @@ public class Car {
         this.series = series;
         this.color = color;
     }
-
     public String getModel() {
         return model;
     }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
     public int getSeries() {
         return series;
     }
 
-    public void setSeries(int series) {
-        this.series = series;
-    }
-
     public String getColor() {
         return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 }
